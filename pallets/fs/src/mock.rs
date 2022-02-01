7592bb2068f0279bfbd4c19ea39a6d9401080324
“ilhanu” <ilhanunlu@gmail.com>
2022-02-01 12:08:53 +0100

    comment mock for temporarily.

--- pallets/fs/src/mock.rs
+++ pallets/fs/src/mock.rs
@@ -10,17 +10,17 @@ use sp_runtime::{
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		FsModule: pallet_fs::{Pallet, Call, Storage, Event<T>},
-	}
-);
+// // Configure a mock runtime to test the pallet.
+// frame_support::construct_runtime!(
+// 	pub enum Test where
+// 		Block = Block,
+// 		NodeBlock = Block,
+// 		UncheckedExtrinsic = UncheckedExtrinsic,
+// 	{
+// 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+// 		FsModule: pallet_fs::{Pallet, Call, Storage, Event<T>},
+// 	}
+// );
 
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
